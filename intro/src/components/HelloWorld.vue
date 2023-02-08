a74cebb7d636e9c6eb96bf55b13b744276858a2c
Sarkhan Abbaszadeh <s.abbaszada@ady.az>
2023-02-08 12:19:31 +0400

    add bootstrap, jquery, popper js

--- intro/src/components/HelloWorld.vue
+++ intro/src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h3 class="alert alert-danger">{{ msg }}</h3>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
