a00586996dddf912f8da7e1a988f4920c91cdba2
Sarkhan Abbaszadeh <s.abbaszada@ady.az>
2023-02-09 10:56:31 +0400

    added table and product list

--- intro/src/components/ProductList.vue
+++ intro/src/components/ProductList.vue
@@ -1,7 +1,29 @@
 <template>
     <div>
         <h3>-- Product List --</h3>
-        {{ products.length }}
+        <p v-if="products.length==0">Product List is empty</p>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Product Name</th>
+                    <th>Category</th>
+                    <th>Description</th>
+                    <th>Unit Price</th>
+                    <th>Unit in Stock</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="product in products" :key="product.id">
+                    <td>{{product.id}}</td>
+                    <td>{{product.productName}}</td>
+                    <td>{{product.categoryId}}</td>
+                    <td>{{product.queantityPerUnit}}</td>
+                    <td>{{product.unitPrice}}</td>
+                    <td>{{product.unitInStock}}</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 
