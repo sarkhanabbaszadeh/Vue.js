8cf740fb4c0a43adf1846e2b38e524d58e717293
Sarkhan Abbaszadeh <s.abbaszada@ady.az>
2023-02-10 10:38:17 +0400

    use v-if & v-else

--- intro/src/components/ProductList.vue
+++ intro/src/components/ProductList.vue
@@ -16,13 +16,26 @@
             </thead>
             <tbody>
                 <tr v-for="product in products" :key="product.id">
-                    <td>{{product.id}}</td>
-                    <td>{{product.productName}}</td>
-                    <td>{{product.categoryId}}</td>
-                    <td>{{product.queantityPerUnit}}</td>
-                    <td>{{product.unitPrice}}</td>
-                    <td>{{product.unitInStock}}</td>
-                    <td><button class="btn btn-sm btn-danger" @click="handleDelete(product)">Delete</button></td>
+                    <td v-if="updateId==product.id"><input v-model="product.id" type="text" class="form-control" id="id"></td>
+                    <td v-else>{{ product.id }}</td>
+                    <td v-if="updateId==product.id"><input v-model="product.productName" type="text" class="form-control" id="id"></td>
+                    <td v-else>{{ product.productName }}</td>
+                    <td v-if="updateId==product.id"><input v-model="product.categoryId" type="text" class="form-control" id="id"></td>
+                    <td v-else>{{ product.categoryId }}</td>
+                    <td v-if="updateId==product.id"><input v-model="product.queantityPerUnit" type="text" class="form-control" id="id"></td>
+                    <td v-else>{{ product.queantityPerUnit }}</td>
+                    <td v-if="updateId==product.id"><input v-model="product.unitPrice" type="text" class="form-control" id="id"></td>
+                    <td v-else>{{ product.unitPrice }}</td>
+                    <td v-if="updateId==product.id"><input v-model="product.unitInStock" type="text" class="form-control" id="id"></td>
+                    <td v-else>{{ product.unitInStock }}</td>
+                    <td v-if="updateId!==product.id">
+                        <button class="btn btn-sm btn-primary" @click="handleUpdate(product)">Edit</button>
+                        <button class="btn btn-sm btn-danger" @click="handleDelete(product)">Delete</button>
+                    </td>
+                    <td v-else>
+                        <button class="btn btn-sm btn-primary" @click="handleUpdate(product)">Update</button>
+                        <button class="btn btn-sm btn-danger" @click="handleDelete(product)">Cancel Update</button>
+                    </td>
                 </tr>
             </tbody>
         </table>
@@ -32,12 +45,18 @@
 <script>
 export default {
     name: "product-list",
+    data(){
+        return {updateId:null};
+    },
     props:{
         products:Array
     },
     methods:{
         handleDelete(product){
             this.$emit("delete:product",product)
+        },
+        handleUpdate(product){
+            this.updateId=product.id
         }
     }
 };
